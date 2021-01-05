1dbac1051e6cf60c25c8dbd3b10349eb13a435e1
Heber <hpietrafeza@autodoc.com.br>
2021-01-05 11:15:52 -0300

    Aula 6 finalizada.

--- Aula - 3-Em-Diante/AddinRibbon/AddinRibbon/Ctr/UcProperties.Designer.cs
+++ Aula - 3-Em-Diante/AddinRibbon/AddinRibbon/Ctr/UcProperties.Designer.cs
@@ -29,45 +29,45 @@ namespace AddinRibbon.Ctr
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tbOut = new System.Windows.Forms.TextBox();
+            this.lbCatogoryName = new System.Windows.Forms.Label();
             this.tbCategoryName = new System.Windows.Forms.TextBox();
             this.tbPropertyName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbPropertyName = new System.Windows.Forms.Label();
             this.tbPropertyValue = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lbPropertyValue = new System.Windows.Forms.Label();
             this.btFind = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbOut
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tbOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(4, 4);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(282, 313);
-            this.textBox1.TabIndex = 0;
+            this.tbOut.Location = new System.Drawing.Point(4, 4);
+            this.tbOut.Margin = new System.Windows.Forms.Padding(5);
+            this.tbOut.Multiline = true;
+            this.tbOut.Name = "tbOut";
+            this.tbOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbOut.Size = new System.Drawing.Size(282, 330);
+            this.tbOut.TabIndex = 0;
             // 
-            // label1
+            // lbCatogoryName
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(3, 336);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Category Name";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbCatogoryName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbCatogoryName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbCatogoryName.Location = new System.Drawing.Point(3, 339);
+            this.lbCatogoryName.Name = "lbCatogoryName";
+            this.lbCatogoryName.Size = new System.Drawing.Size(108, 20);
+            this.lbCatogoryName.TabIndex = 1;
+            this.lbCatogoryName.Text = "Category Name";
+            this.lbCatogoryName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // tbCategoryName
             // 
             this.tbCategoryName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbCategoryName.Location = new System.Drawing.Point(117, 336);
+            this.tbCategoryName.Location = new System.Drawing.Point(117, 339);
             this.tbCategoryName.Name = "tbCategoryName";
             this.tbCategoryName.Size = new System.Drawing.Size(169, 20);
             this.tbCategoryName.TabIndex = 2;
@@ -76,45 +76,46 @@ namespace AddinRibbon.Ctr
             // 
             this.tbPropertyName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbPropertyName.Location = new System.Drawing.Point(117, 362);
+            this.tbPropertyName.Location = new System.Drawing.Point(117, 365);
             this.tbPropertyName.Name = "tbPropertyName";
             this.tbPropertyName.Size = new System.Drawing.Size(169, 20);
             this.tbPropertyName.TabIndex = 4;
             // 
-            // label2
+            // lbPropertyName
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(3, 362);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Property Name";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbPropertyName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbPropertyName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbPropertyName.Location = new System.Drawing.Point(3, 365);
+            this.lbPropertyName.Name = "lbPropertyName";
+            this.lbPropertyName.Size = new System.Drawing.Size(108, 20);
+            this.lbPropertyName.TabIndex = 3;
+            this.lbPropertyName.Text = "Property Name";
+            this.lbPropertyName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // tbPropertyValue
             // 
             this.tbPropertyValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbPropertyValue.Location = new System.Drawing.Point(117, 388);
+            this.tbPropertyValue.Location = new System.Drawing.Point(117, 391);
             this.tbPropertyValue.Name = "tbPropertyValue";
             this.tbPropertyValue.Size = new System.Drawing.Size(169, 20);
             this.tbPropertyValue.TabIndex = 6;
             // 
-            // label3
+            // lbPropertyValue
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(3, 388);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Property Value";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbPropertyValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbPropertyValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbPropertyValue.Location = new System.Drawing.Point(3, 391);
+            this.lbPropertyValue.Name = "lbPropertyValue";
+            this.lbPropertyValue.Size = new System.Drawing.Size(108, 20);
+            this.lbPropertyValue.TabIndex = 5;
+            this.lbPropertyValue.Text = "Property Value";
+            this.lbPropertyValue.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btFind
             // 
-            this.btFind.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btFind.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.btFind.Location = new System.Drawing.Point(0, 415);
             this.btFind.Name = "btFind";
             this.btFind.Size = new System.Drawing.Size(289, 23);
@@ -129,12 +130,12 @@ namespace AddinRibbon.Ctr
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btFind);
             this.Controls.Add(this.tbPropertyValue);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbPropertyValue);
             this.Controls.Add(this.tbPropertyName);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbPropertyName);
             this.Controls.Add(this.tbCategoryName);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbCatogoryName);
+            this.Controls.Add(this.tbOut);
             this.Name = "UcProperties";
             this.Size = new System.Drawing.Size(289, 438);
             this.ResumeLayout(false);
@@ -144,13 +145,13 @@ namespace AddinRibbon.Ctr
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbOut;
+        private System.Windows.Forms.Label lbCatogoryName;
         private System.Windows.Forms.TextBox tbCategoryName;
         private System.Windows.Forms.TextBox tbPropertyName;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbPropertyName;
         private System.Windows.Forms.TextBox tbPropertyValue;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbPropertyValue;
         private System.Windows.Forms.Button btFind;
     }
 }
--- Aula - 3-Em-Diante/AddinRibbon/AddinRibbon/Ctr/UcProperties.cs
+++ Aula - 3-Em-Diante/AddinRibbon/AddinRibbon/Ctr/UcProperties.cs
@@ -15,10 +15,67 @@ namespace AddinRibbon.Ctr
         public UcProperties()
         {
             InitializeComponent();
+
+            this.ListenSelection(null, null);
+            Autodesk.Navisworks.Api.Application.ActiveDocumentChanged += ListenSelection;
         }
 
         /// <summary>
-        /// Aula 6
+        /// Aula 6 propriedades
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListenSelection(object sender, EventArgs e)
+        {
+            try
+            {
+                Autodesk.Navisworks.Api.Application.ActiveDocument.CurrentSelection.Changed += GetProperties;
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            
+        }
+
+        /// <summary>
+        /// Aula 6 propriedades
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GetProperties(object sender, EventArgs e)
+        {
+            this.tbOut.Clear();
+            List<string> result = new List<string>();
+            //Percorre itens selecionados
+            foreach (var item in Autodesk.Navisworks.Api.Application.ActiveDocument.CurrentSelection.SelectedItems)
+            {
+                result.Add(item.DisplayName);
+                //Percorre categorias de propriedades do elementos selecionados
+                foreach (var cat in item.PropertyCategories)
+                {
+                    result.Add(string.Concat(".     ", cat.DisplayName));
+
+                    //Percorre propriedades das categorias dos elementos selecionados
+                    foreach (var prop in cat.Properties)
+                    {
+                        result.Add(string.Concat(".     .       ", prop.DisplayName, "> ", GetPropertyValeu(prop)));
+                    }
+                }
+
+                result.Add(Environment.NewLine);
+            }
+
+            this.tbOut.Text = string.Join(Environment.NewLine, result);
+        }
+
+        private string GetPropertyValeu(Autodesk.Navisworks.Api.DataProperty prop)
+        {
+            return prop.Value.IsDisplayString ? prop.Value.ToDisplayString() : prop.Value.ToString().Split(':')[1];
+        }
+
+        /// <summary>
+        /// Aula 6 propriedades
         /// </summary>
         /// <param name="e"></param>
         protected override void OnParentChanged(EventArgs e)
@@ -28,7 +85,7 @@ namespace AddinRibbon.Ctr
         }
 
         /// <summary>
-        /// Aula 6
+        /// Aula 6 propriedades
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
